47fcffa2bcd461f9c9639acf94b13c8f227aaf3e
deadend135 <sharma.vikrant135@gmail.com>
2020-11-25 21:16:52 +0530

    initial push

---
+++ src/components/carousel/BootstrapCarousel.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import Carousel from "react-bootstrap/Carousel";
+import '../../App.css'
+export class BootstrapCarousel extends Component {
+  render() {
+    return (
+      <AliceCarousel
+        autoPlay
+        autoPlayInterval={3000}
+        animationDuration={3000}
+        animationType="slide"
+        infinite
+        mouseTracking
+        touchTracking={false}
+        disableDotsControls="false"
+        disableButtonsControls="true"
+        items={items}
+      />
+    );
+  }
+}
+const handleDragStart = (e) => e.preventDefault();
+const items = [
+  <div>
+    <img
+      src="assets/img/img1.jpg"
+      onDragStart={handleDragStart}
+      className="sliderimg"
+    />
+    <div class="text-block">
+      <h1>THANKFUL FOR FREE DELIVERY</h1>
+      <p>Score free delivery on any size order for a limited time</p>
+    </div>
+  </div>,
+  <img
+    src="assets/img/img2.jpg"
+    onDragStart={handleDragStart}
+    className="sliderimg"
+  />,
+  <img
+    src="assets/img/img3.jpg"
+    onDragStart={handleDragStart}
+    className="sliderimg"
+  />,
+];
+
+export default BootstrapCarousel;
